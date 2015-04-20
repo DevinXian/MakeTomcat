6999d9510e3217c0186d53573714c710cc8f96aa
DevinXian <DevinXian@gmail.com>
2015-04-20 11:44:08 +0800

    Simple Socket Programming

---
+++ src/net/wind/ch01/client/MyClient.java
@@ -0,0 +1,43 @@
+package net.wind.ch01.client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MyClient {
+
+	public static void main(String[] args) throws Exception {
+		Socket socket = new Socket("127.0.0.1", 8080);
+		OutputStream os = socket.getOutputStream();
+		boolean autoFlush = true;
+		PrintWriter out = new PrintWriter(os, autoFlush);
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				socket.getInputStream()));
+
+		// send an HTTP request to the web server
+		out.println("GET /index.html HTTP/1.1");
+		out.println("Host: localhost:8080");
+
+		out.println("Connection: Close");
+		out.println();
+
+		// read the response
+		boolean loop = true;
+		StringBuffer sb = new StringBuffer(8096);
+		while (loop) {
+			if (in.ready()) {
+				int i = 0;
+				while (i != -1) {
+					i = in.read();
+					sb.append((char) i);
+				}
+				loop = false;
+			}
+			Thread.sleep(50);
+		}
+		System.out.println(sb.toString());
+		socket.close();
+	}
+}
